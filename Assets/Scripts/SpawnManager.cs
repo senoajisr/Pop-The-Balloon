18f616a9d0d2889f296d5481e9541c58274d9cce
Raditya Ega <radityaega@student.telkomuniversity.ac.id>
2023-06-14 02:25:25 +0700

    Update Fix

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,7 +8,7 @@ public class SpawnManager : MonoBehaviour {
     private float spawnDelay = 10;
     private float spawnDelayReduction = 0.1f;
     private float currentSpawnDelay;
-    private List<Vector3> spawnRange = new List<Vector3> {new Vector3(-10, 25, 13), new Vector3(10, 1.5f, 13)};
+    private List<Vector3> spawnRange = new List<Vector3> {new Vector3(-10, 30, 13), new Vector3(10, 1.5f, 13)};
 
     // Start is called before the first frame update
     void Start() {
